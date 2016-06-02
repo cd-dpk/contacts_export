efb11a32ce431efd1f41351acca08551f296ce9c
Dipok Chandra Das <chandradasdipok@gmail.com>
2016-06-02 10:53:40 +0600

    adding AsyncTask class in TemplateActivity

--- app/src/main/java/com/example/user/contacts_export/activities/TemplateActivity.java
+++ app/src/main/java/com/example/user/contacts_export/activities/TemplateActivity.java
@@ -1,25 +1,63 @@
 package com.example.user.contacts_export.activities;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-
 public abstract class TemplateActivity extends AppCompatActivity {
 
+     /**
+     *  init the view
+     */
     public abstract void  initView();
+    /**
+     * load the data
+     */
     public abstract void loadData();
+    /**
+     * it initializes the view with loading data
+     */
     public abstract void initializeView();
+
+    /**
+     * it listen the view
+     */
     public abstract void listenView();
 
-    @Override
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initView();
-        loadData();
-        initializeView();
-        listenView();
+        new CustomAsyncTask().execute();
+    }
+
+    class CustomAsyncTask extends AsyncTask<Void,Void,Void>{
+
+        ProgressDialog progressDialog = new ProgressDialog(TemplateActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            initView();
+            progressDialog.setTitle("Messasge");
+            progressDialog.setMessage("Initializing view");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            loadData();
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if (progressDialog.isShowing()){
+                progressDialog.dismiss();
+            }
+            initializeView();
+            listenView();
+        }
     }
 
 }
